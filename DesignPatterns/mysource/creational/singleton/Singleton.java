e875161a4b7dbfe95ffc08b391f30144261a1ea2
Mahto, Ajesh <ajesh.mahto@anthem.com>
2017-07-07 16:44:56 -0700

    update

---
+++ DesignPatterns/mysource/creational/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package creational.singleton;
+
+public class Singleton {
+	
+	private static Singleton instance=null;
+	
+	private Singleton(){}
+	
+	public static synchronized Singleton getInstance() {
+		if(instance==null){
+			return instance = new Singleton();
+		}
+		
+		return instance;
+	}
+	
+	public void print(){
+		System.out.println("Hello I am singleton");
+	}
+
+}
